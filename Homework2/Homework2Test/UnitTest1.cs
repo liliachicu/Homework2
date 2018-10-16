2967cd55f23ba5dfca1444f5cd750b99d0910fd3
lchicu <lchicu@EN32193>
2018-10-16 11:15:44 +0300

    ccbv

--- Homework2/Homework2Test/UnitTest1.cs
+++ Homework2/Homework2Test/UnitTest1.cs
@@ -13,8 +13,6 @@ namespace Homework2Test
         {
             var report = new ReportGenerator();
             var actual = report.GenerateReport("1 Lilia Chicu 5000 10/10/2018;2 Dorin Mocan 4500 09/10/2018;3 Eugen Stratulat 5005 09/01/2017;4 Dorina Dumbrava 6000 08/09/2017");
-            
-       
         }
     }
 }
